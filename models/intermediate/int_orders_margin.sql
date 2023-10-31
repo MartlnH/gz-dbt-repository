d29cde1e631b335be75e74e640e5dc1507c7536a
haumartin0@gmail.com <haumartin0@gmail.com>
2023-10-31 13:40:04 +0000

    build finance 2

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -2,7 +2,7 @@ select
 date_date,
 orders_id,
 count(orders_id) as nb_orders
-sum(revenue) as revenue
+, sum(revenue) as revenue
 , round(sum (purchase_cost),2) as purchase_cost
 , round(sum(margin),2) as margin_per_order
 from {{ref('int_sales_margin')}}
